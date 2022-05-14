7b5ef35bfdf58434dbc0519f038a2d990187d8f4
sagor devnath <ssagor64@gmail.com>
2022-05-14 09:41:24 +0600

    navbar structured

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Services from "./pages/Services";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>     
+      <Navbar>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/services' element={<Services />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+      </Routes>
+      </Navbar>
     </div>
   );
 }
